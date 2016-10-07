89d557d77d303b7b1f921e75c3e078a71486fa5b
emottk <emily.kowal@postlight.com>
2016-10-07 11:17:55 -0400

    update lab

---
+++ lab.rb
@@ -0,0 +1,25 @@
+class Person(name, age, height)
+
+#make it so you can access name, age, and height
+
+#make it so you can change the age and the height
+
+#create a method that let's this person walk
+
+#create a method that let's this person sing
+
+#create a method that will print "I LOVE LIFE!" if the person's mood is "happy"
+  #and "I'm having a tough day" if the person's mood is sad
+
+#create a method that will take in a date and say "TODAY IS MY BIRTHDAY!"
+  #if the input is "01/11". If the input is "01/11", add one year to the Person's age.
+
+
+end
+
+my_person = Person.new("Bob", 35, "6'2")
+my_person.name
+my_person.age
+my_person.height = "6'3"
+
+#call the rest of your methods here!
